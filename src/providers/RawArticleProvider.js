c666f22e5f186dac31279f85cb217180245e107b
Niels Joubert <njoubert@gmail.com>
2013-06-26 22:43:34 -0700

    Stubbed out reading json files

--- src/providers/RawArticleProvider.js
+++ src/providers/RawArticleProvider.js
@@ -1,4 +1,5 @@
-var fs = require('fs');
+var fs   = require('fs');
+var path = require('path');
 
 /*
  * This module parses a directory full of raw .json files
@@ -53,11 +54,26 @@ RawArticleProvider.prototype.getTitles = function(callback) {
  * This loads a directory full of json files,
  * discarding those that doesn't conform to our format,
  * and filling in details as necessary.
+ *
+ * Potential Issues: If files are busy loading when a request gets made, 
+ * only those loaded so far will be returned. Possibly notify the user?
  */
 RawArticleProvider.prototype.loadDir = function(dirname) {
   console.log("Loading articles from", dirname);
 
+  function loadFile(err,data) {
+    //Here we parse the json
+  }
 
+  fs.readdir(dirname, function(err, files) {
+
+    for (i in files) {
+      if (path.extname(files[i]) == '.json') {
+        fs.readFile(path.join(dirname, files[i]), loadFile);
+      }
+    }
+
+  });
 } 
 
 module.exports = RawArticleProvider;
