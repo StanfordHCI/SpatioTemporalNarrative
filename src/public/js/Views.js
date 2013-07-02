af48b85f333c2f9b8b929835b23aa59338b30728
Niels Joubert <njoubert@gmail.com>
2013-07-01 19:45:51 -0700

    Adding comments to describe design

--- src/public/js/Views.js
+++ src/public/js/Views.js
@@ -12,24 +12,33 @@ Views = (function() {
 
   _.extend(ArticleIndexView.prototype, Backbone.Events, {
     
+    //Initializes the view by binding change events from the model to the render function
     initialize: function() {
       this.listenTo(this.model, "change", _.bind(this.render, this)); 
     }, 
 
+    //Draws the view into the element.
     render: function() {
       this.el.html(this.template(this.model.get()));
+      return this;
     }, 
     
     clear: function() {
       this.el.html("");
+      return this;
     },
 
+    //This checks that we were handed an element to render to.
     _ensureElement: function() {
       if (!this.el) {
         throw new Error("View requires a container element");
       }
     },
 
+    //This sets up all the event listeners, only runs on initialization.
+    //It uses event delegation, so listeners only occur
+    //on the element of this entire View, not any subelements.
+    //This means that we do not have to rebind events on every render
     _delegateEvents: function() {
 
       var self = this;
@@ -45,6 +54,7 @@ Views = (function() {
 
     },
 
+    //This stores the template HTML to render for the view
     template: _.template("<ul><% _.each(articles, function(article){ %> <li articleId=<%= article.id %>> <%= article.title %> </li> <% }) %> </ul>")
   });
 
