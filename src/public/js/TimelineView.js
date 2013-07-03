3f5ade8faba06b0f5a173c1854f0435174ee6867
alt2014 <anhlt92@stanford.edu>
2013-07-03 15:17:55 -0700

    implemented nonlinear transforms and fixed scroll interaction

--- src/public/js/TimelineView.js
+++ src/public/js/TimelineView.js
@@ -77,25 +77,12 @@ TimelineView = (function() {
         markerSet.data("id", evts[i].id);
         markerSet.click(function() {
           console.log(this.data("id"));
-          modelView.scrollHasReached(this.data("id"));          
+          modelView.scrollHasReached(this.data("id")); 
         });
 
         console.log(markerSet);
         timeline.events.push({id: evts[i].id, marker: markerSet, time: times});
         
-        /*
-        if (evts[i].events) {
-          for (var j = 0; j < evts[i].events.length; j++) {
-            if (evts[i].events[j].time) {
-              var subStart = evts[i].events[j].time[0];
-              var subEnd = evts[i].events[j].time[evts[i].events[j].time.length - 1];
-              console.log(evts[i].events[j].title);
-              drawTimeBlock(this.paper, range, start_time, line_length, line_width, x_offset, y_offset, subStart, subEnd, "sub", this.modelView, evts[i].events[j]);
-            }
-
-          }
-          //*/
-        //}
       }
       linearTransform(timeline);
       return this;
@@ -104,9 +91,9 @@ TimelineView = (function() {
 
 
     renderScrolled: function(event) {
-      for (var i = 0; i < this.events.length; i++){
+      for (var i = 0; i < this.timeline.events.length; i++){
         
-        var evt = this.events[i];
+        var evt = this.timeline.events[i];
 
         if(event.id == evt.id) {
           evt.marker.attr("fill", "#fff");
@@ -158,44 +145,24 @@ TimelineView = (function() {
     Takes in the timeline object and returns it with an updated arrtay of points that have been transformed such that they
     are equally spaced 
   */
-  function nonlinearTransform(obj, events) {
+  function nonlinearTransform(timeline) {
+    var events = timeline.events;
+
+    var spacing = timeline.absLen / events.length;
+
+    for (var i = 0; i < events.length; i++) {
+      var offsetFromStart = spacing * i;
+      var properties = {
+        cx: timeline.xOffset,
+        cy: (timeline.yOffset + offsetFromStart)
+      };
+      events[i].marker.animate(properties, 3000, "linear");
+    }
 
   }
 
   function scale(tlRange, tlLen, point1, point2) {
     return (tlLen * (point2 - point1)/tlRange);
-  }
-
-  function drawTimeBlock(paper, tlRange, tlStart, tlLen, tlWidth, tlXOff, tlYOff, curStart, curEnd, type, model, evt) {
-    var start_time = new Date(curStart);
-    var end_time = new Date(curEnd);
-    var block_length = (tlLen * (end_time - start_time)/tlRange);
-    block_length == 0? block_length++ : block_length;
-    var block_offset = tlLen * (start_time - tlStart)/tlRange;
-    var block = paper.rect(tlXOff, tlYOff + block_offset, tlWidth, block_length);
-    block.attr("stroke", "#fff");
-    if (type == "main") {
-      var circ = paper.circle(tlXOff + tlWidth/2, tlYOff + block_offset, 5);
-      circ.attr("fill", "#A0A0A0");
-      circ.attr("stroke", "#fff");
-      circ.data("id", evt.id);
-      circ.click(function(){
-        eve("timeChange", evt.id);
-        //make this a different color
-        this.attr("fill", "Red");
-        model.scrollHasReached(evt.id);
-
-
-        return this;
-
-      });
-      return circ;
-    } else {
-      var pathStr = "M";
-      pathStr += tlXOff + " " + (tlYOff + block_offset) + "H" + (tlXOff + tlWidth); 
-      paper.path(pathStr).attr("stroke-width", ".5");
-    }
-    //var label = paper.text(10, tlYOff + block_offset, curStart);
   }
 
 
