3bfbf27d7075797af2afea4e0702c0c357212c0e
alt2014 <anhlt92@stanford.edu>
2013-07-02 17:02:38 -0700

    Initial timeline displays all points

--- src/public/js/TimelineView.js
+++ src/public/js/TimelineView.js
@@ -28,17 +28,50 @@ TimelineView = (function() {
     },
 
     renderFromScratch: function() {
-      
-      var paper = this.paper = Raphael(this.el, 60, 500);
-      var circle = paper.circle(20, 40, 10);
-      circle.attr("fill", "#f00");
-      circle.attr("stroke", "#fff");
-      circle.touchmove(function(evt) {
-        
-      })
 
+      var line_length = 600;
+      var x_offset = 40;
+      var line_width = 5;
+      var y_offset = 10;
+
+      var paper = this.paper = Raphael(this.el, 60, 700);
+      var line = paper.rect(x_offset, y_offset, line_width,  line_length, 5);
+      line.attr("fill", "#A0A0A0");
+      line.attr("stroke", "#fff");
+
+      var evts = this.model.get("events");
+      var start_time = new Date(evts[0].time[0]);
+      var last_evt = evts[evts.length - 1];
+      var end_time =  new Date(last_evt.time[last_evt.time.length - 1]);
+      var range = end_time.getTime() - start_time.getTime();
+      var last_time = start_time;
+      
+      for (var i = 0; i < evts.length; i++) {
+        var current_time = evts[i].time[0];
+        var current_end_time = evts[i].time[evts[i].time.length - 1];
+        drawTimeBlock(this.paper, range, start_time, line_length, line_width, x_offset, y_offset, current_time, current_end_time);
+        
+        if (evts[i].events) {
+          for (var j = 0; j < evts[i].events.length; j++) {
+            if (evts[i].events[j].time) {
+              var subStart = evts[i].events[j].time[0];
+              var subEnd = evts[i].events[j].time[evts[i].events[j].time.length - 1];
+              console.log(evts[i].events[j].title);
+              drawTimeBlock(this.paper, range, start_time, line_length, line_width, x_offset, y_offset, subStart, subEnd);
+            }
+
+          }
+        }
+      };
+
+
+      
+      //get the first and last times to figure out the range
+      //for each event and subevent, iterate through and draw a rectangle
+      //draw a circle and add event listeners to it 
       return this;
     }, 
+
 
     renderScrolled: function(event) {
       this.el.innerHTML = event.time;
@@ -58,6 +91,16 @@ TimelineView = (function() {
   });
 
   return TimelineView;
+
+  function drawTimeBlock(paper, tlRange, tlStart, tlLen, tlWidth, tlXOff, tlYOff, curStart, curEnd) {
+    var start_time = new Date(curStart);
+    var end_time = new Date(curEnd);
+    var block_length = (tlLen * (end_time - start_time)/tlRange) + 1;
+    var block_offset = tlLen * (start_time - tlStart)/tlRange;
+    var block = paper.rect(tlXOff, tlYOff + block_offset, tlWidth, block_length);
+    block.attr("stroke", "#fff");
+    var label = paper.text(10, tlYOff + block_offset, curStart);
+  }
 
 
 
